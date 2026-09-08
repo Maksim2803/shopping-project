dab599302ba159959c35c3a4075a0b3ac7e1b33a
Максим <maksimphomich321@gmail.com>
2026-09-08 14:05:09 +0300

    Исправил MenuManager

--- bot/MenuManager.py
+++ bot/MenuManager.py
@@ -1,11 +1,10 @@
 class MenuManager:
-    def __init__(self,board):
-        self.board=board
+    def __init__(self):
         self.admin_buttons={"start":[["Очистить список","Полный список"],["Добавить кнопку","Удалить кнопку"]],"categories":[["Назад"]]}
 
-    def creation_button(self):
+    def creation_button(self,board):
         new_keyboard = [[]]
-        board=list(self.board.keys())
+        board=list(board.keys())
         board.append("Стоп")
         for i in board:
             for row in new_keyboard:
@@ -16,13 +15,13 @@ class MenuManager:
                 new_keyboard.append([i])
         return new_keyboard
 
-    def add_button_part1(self,button,category):
+    def add_button_part1(self,button,category,board):
         if not category:
             return "category not selected"
-        button=self.name_check(button,category)
+        button=self.name_check(button,category,board)
         if button=="name is taken" or button=="name name unavailable":
             return button
-        board_spisok=self.unpacking(self.board[category])
+        board_spisok=self.unpacking(board[category])
         admin_spisok=self.unpacking(self.admin_buttons)
         new_board=[]
         for i in range(len(board_spisok)):
@@ -31,9 +30,9 @@ class MenuManager:
             else:
                 new_board.append(board_spisok[i])
         new_board.append(button)
-        return self.add_button_part2(new_board,button,category)
+        return self.add_button_part2(new_board,button,category,board)
 
-    def add_button_part2(self,new_board,button,category):
+    def add_button_part2(self,new_board,button,category,board):
         custom_buttons=[[]]
         for i in new_board:
             for row in custom_buttons:
@@ -44,15 +43,15 @@ class MenuManager:
                 custom_buttons.append([i])
         if category=="Главное меню":
             custom_buttons.extend(self.admin_buttons["start"])
-            self.board[button]=[["Назад"]]
+            board[button]=[["Назад"]]
         else:
             custom_buttons.extend(self.admin_buttons["categories"])
-        self.board[category]=custom_buttons
+        board[category]=custom_buttons
         return category
 
-    def delete_buttons(self,button,category):
+    def delete_buttons(self,button,category,board):
         try:
-            board_spisok=self.board[category]
+            board_spisok=board[category]
         except KeyError:
             return "KeyError"
         if self.check_delete_buttons(button)=="prohibited for removal":
@@ -61,8 +60,8 @@ class MenuManager:
             for b in i:
                 if b==button:
                     i.remove(b)
-                    self.board.pop(b,None)
-        self.board[category]=board_spisok
+                    board.pop(b,None)
+        board[category]=board_spisok
         return category
 
     def check_delete_buttons(self,button):
@@ -92,8 +91,8 @@ class MenuManager:
         else:
             return [spisok]
 
-    def name_check(self,name,category):
-        board_spisok=self.unpacking(self.board[category])
+    def name_check(self,name,category,board):
+        board_spisok=self.unpacking(board[category])
         admin_spisok=self.unpacking(self.admin_buttons)
         if name in board_spisok:
             return "name is taken"
